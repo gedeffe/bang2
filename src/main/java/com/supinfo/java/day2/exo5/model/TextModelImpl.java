d164406b0b33fcdbda3aa7dce4a2a2a18be0e4b2
Gabriel Barbier <amelioretonparent@gmail.com>
2022-01-17 13:30:55 +0100

    Add database persistence.
    Use H2 as embedded database in build folder.

--- src/main/java/com/supinfo/java/day2/exo5/model/TextModelImpl.java
+++ src/main/java/com/supinfo/java/day2/exo5/model/TextModelImpl.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,35 +14,56 @@ import java.util.List;
 public class TextModelImpl implements TextModel {
 
     private final List<TextEvents> listeners = new ArrayList<>();
+    private final Connection connection;
 
-    /*
-    Use a file located in build folder to persist information.
+    /**
+     * Default constructor to initialize connection to database
      */
-    private final Path dummyDatabase = Path.of("build", "dummyDatabase.txt");
-
-    @Override
-    public void updateTextData(final String text) {
-        // update our persistence layer
-        this.writeTextToFile(text);
-        // send a events to update listeners
-        this.listeners.forEach((listener) -> listener.notifyTextModified(text));
-    }
-
-    private void writeTextToFile(String text) {
-        try {
-            // replace current text
-            Files.writeString(this.dummyDatabase, text);
-        } catch (IOException e) {
-            e.printStackTrace();
+    public TextModelImpl() throws ClassNotFoundException, SQLException {
+        // load H2 driver
+        Class.forName("org.h2.Driver");
+        // Start database connection
+        String url = "jdbc:h2:./build/h2";
+        this.connection = DriverManager.getConnection(url, "sa", "");
+        try (Statement statement = connection.createStatement()) {
+            // create our table structure (if needed) to store our text
+            String sqlCreateCommand = "CREATE TABLE IF NOT EXISTS supinfo (text VARCHAR(1500) NOT NULL);";
+            statement.executeUpdate(sqlCreateCommand);
+            ResultSet resultSet = statement.executeQuery("SELECT text FROM supinfo;");
+            if (!resultSet.next()) {
+                // insert a default value
+                String sqlInsertCommand = "INSERT INTO supinfo VALUES ('initial value');";
+                statement.executeUpdate(sqlInsertCommand);
+            }
         }
     }
 
     @Override
+    public void updateTextData(final String text) {
+        // update our persistence layer
+        this.writeTextToDatabase(text);
+        // send a events to update listeners
+        this.listeners.forEach((listener) -> listener.notifyTextModified(text));
+    }
+
+    private void writeTextToDatabase(String text) {
+        try (Statement statement = this.connection.createStatement()) {
+            statement.executeUpdate("UPDATE supinfo SET text='" + text +"';");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
     public String getTextData() {
         String text = "";
-        try {
-            text = Files.readString(this.dummyDatabase);
-        } catch (IOException e) {
+        try (Statement statement = this.connection.createStatement()){
+            ResultSet resultSet = statement.executeQuery("SELECT text FROM supinfo;");
+            while(resultSet.next()) {
+                text = resultSet.getString("text");
+            }
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return text;
