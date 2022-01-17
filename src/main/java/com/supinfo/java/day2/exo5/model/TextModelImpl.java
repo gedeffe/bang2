08c80b841122928e6fb883ecb6a5645215c98ee6
Gabriel Barbier <amelioretonparent@gmail.com>
2022-01-17 10:25:30 +0100

    Add first level of persistence...
    Only a dummy file, to reflect console state.

--- src/main/java/com/supinfo/java/day2/exo5/model/TextModelImpl.java
+++ src/main/java/com/supinfo/java/day2/exo5/model/TextModelImpl.java
@@ -1,5 +1,9 @@
 package com.supinfo.java.day2.exo5.model;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,13 +15,29 @@ public class TextModelImpl implements TextModel {
     private final List<TextEvents> listeners = new ArrayList<>();
     private String textData = "";
 
+    /*
+    Use a file located in build folder to persist information.
+     */
+    private final File dummyDatabase = new File("build/dummyDatabase.txt");
+
     @Override
     public void updateTextData(final String text) {
         // here we might store our data to a persistence layer
         // won't do it for this tiny example
         this.textData = text;
+        // update our persistence layer
+        this.writeTextToFile(text);
         // send a events to update listeners
         this.listeners.forEach((listener) -> listener.notifyTextModified(text));
+    }
+
+    private void writeTextToFile(String text) {
+        try (Writer writer = new FileWriter(dummyDatabase)) {
+            // replace current text
+            writer.write(text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
