2eedd9bada77a7f21c1e63a7465cc53f89515773
Pierre Van Maele <pierre.van-maele@supinfo.com>
2023-01-13 13:32:04 +0100

    front(feat) creation de la barre et du bouton : send, pour envoyer les messages

--- src/main/java/com/supinfo/whatzappeuh/WhatzappeuhPrincipal.java
+++ src/main/java/com/supinfo/whatzappeuh/WhatzappeuhPrincipal.java
@@ -1,15 +1,23 @@
 package com.supinfo.whatzappeuh;
+//package com.supinfo.whatzappeuh.UserSendingMessageInterface;
 
 import javax.swing.*;
 
 public class WhatzappeuhPrincipal {
 
+    //private SendingMessageInterface sendingMessageInterface = new SendingMessageInterface();
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("Whatzappeuh");
+            UserSendingMessageInterface placeView = new UserSendingMessageInterface();
 
-            frame.pack();
-            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            frame.add(placeView.sendingMessageInterface());
+            //frame.pack();
+
+            frame.setSize(300,100);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            //frame.setLayout(null);
             frame.setVisible(true);
         });
     }
