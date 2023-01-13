b50b551ed7cc0a0c5e02cfcfbb8d91531ead7e4c
Baptiste Georget <baptiste.georget@hotmail.com>
2023-01-13 14:31:27 +0100

    View account manager

---
+++ src/main/java/com/supinfo/whatzappeuh/AccountView.java
@@ -0,0 +1,46 @@
+package com.supinfo.whatzappeuh;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class AccountView {
+
+    public static void display(JFrame mainFrame) {
+        JDialog frame = new JDialog(mainFrame, "Account manager", true);
+        frame.setSize(550, 500);
+        frame.setLocationRelativeTo(null);
+        JPanel panel = panel();
+        frame.setContentPane(panel);
+        frame.setVisible(true);
+    }
+
+    private static JPanel panel() {
+        // Panels
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        JPanel panelLeft = new JPanel();
+        panelLeft.setLayout(new BoxLayout(panelLeft, BoxLayout.Y_AXIS));
+        mainPanel.add(panelLeft);
+        JPanel panelRight = new JPanel();
+        panelRight.setLayout(new GridLayout(0,1));
+        mainPanel.add(panelRight);
+
+        // Table
+        Object[] header = { "test" };
+        Object[][] data = { { "test" } };
+        JTable table = new JTable(data, header);
+        JScrollPane scrollPanel = new JScrollPane(table);
+        panelLeft.add(scrollPanel);
+
+        // Button +
+        JButton add = new JButton("+");
+        panelRight.add(add);
+
+        // Button -
+        JButton remove = new JButton("-");
+        panelRight.add(remove);
+
+        return mainPanel;
+    }
+
+}
--- src/main/java/com/supinfo/whatzappeuh/WhatzappeuhPrincipal.java
+++ src/main/java/com/supinfo/whatzappeuh/WhatzappeuhPrincipal.java
@@ -6,10 +6,27 @@ public class WhatzappeuhPrincipal {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
+            // Frame
             JFrame frame = new JFrame("Whatzappeuh");
-
-            frame.pack();
+            frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+            frame.setLocationRelativeTo(null);
             frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+            // MenuBar
+            JMenuBar menuBar = new JMenuBar();
+            frame.setJMenuBar(menuBar);
+
+            // Menu
+            JMenu menu = new JMenu("Menu");
+            menuBar.add(menu);
+
+            //MenuItems
+            JMenuItem accountManagement = new JMenuItem("Account management");
+            JMenuItem selectRecipient = new JMenuItem("Select recipient");
+            menu.add(accountManagement);
+            accountManagement.addActionListener((actionEvent)->AccountView.display(frame));
+            menu.add(selectRecipient);
+
             frame.setVisible(true);
         });
     }
