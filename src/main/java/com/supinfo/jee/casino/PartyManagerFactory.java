e3d361c77dbeb8577012947f01e92a4c32f18c27
gedeffe <barbierg.gabriel@gmail.com>
2023-05-22 11:57:58 +0200

    add dependency injection with spring

--- src/main/java/com/supinfo/jee/casino/PartyManagerFactory.java
+++
@@ -1,8 +0,0 @@
-package com.supinfo.jee.casino;
-
-public class PartyManagerFactory {
-
-    public static PartyManager getPartyManager() {
-        return new PartyManagerImpl();
-    }
-}
--- src/main/java/com/supinfo/jee/casino/Princ.java
+++ src/main/java/com/supinfo/jee/casino/Princ.java
@@ -1,9 +1,14 @@
 package com.supinfo.jee.casino;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Princ {
 
     public static void main(String[] args) {
-        PartyManager partyManager = PartyManagerFactory.getPartyManager();
+
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("services.xml");
+        PartyManager partyManager = applicationContext.getBean(PartyManager.class);
 
         System.out.println(partyManager.newParty());
     }
