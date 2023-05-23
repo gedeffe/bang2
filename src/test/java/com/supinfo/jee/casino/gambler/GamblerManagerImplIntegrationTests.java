87d2b44e44f3f80d8bbc156251b49c6c5f200323
gedeffe <barbierg.gabriel@gmail.com>
2023-05-23 09:24:35 +0200

    Add integration test for GamblerManager.

---
+++ src/test/java/com/supinfo/jee/casino/gambler/GamblerManagerImplIntegrationTests.java
@@ -0,0 +1,39 @@
+package com.supinfo.jee.casino.gambler;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamblerManagerImplIntegrationTests {
+
+    @Autowired
+    private GamblerManager gamblerManager;
+
+    @Test
+    void whenGetGambler_givenExistingPseudo() {
+        // Given
+        String pseudo = "pigeon";
+
+        // When
+        Gambler gambler = this.gamblerManager.getGambler(pseudo);
+
+        // Then
+        Assertions.assertThat(gambler).isNotNull().hasFieldOrPropertyWithValue("pseudo", pseudo)
+                .hasFieldOrPropertyWithValue("balance", 5000L).hasFieldOrPropertyWithValue("bet", 25);
+    }
+
+    @Test
+    void whenGetGambler_givenUnknownPseudo() {
+        // Given
+        String pseudo = "unknown";
+
+        // When
+        Gambler gambler = this.gamblerManager.getGambler(pseudo);
+
+        // Then
+        Assertions.assertThat(gambler).isNotNull().hasFieldOrPropertyWithValue("pseudo", pseudo)
+                .hasFieldOrPropertyWithValue("balance", 0L).hasFieldOrPropertyWithValue("bet", 0);
+    }
+}
