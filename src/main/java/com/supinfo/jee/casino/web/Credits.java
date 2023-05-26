ff3f89fb7179f6b17e73f5d71a414d6fbc9ddc9f
gedeffe <barbierg.gabriel@gmail.com>
2023-05-26 09:27:37 +0200

    :fire: add bootstrap for a beautiful page

---
+++ src/main/java/com/supinfo/jee/casino/web/Credits.java
@@ -0,0 +1,10 @@
+package com.supinfo.jee.casino.web;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Credits {
+    private int amount;
+}
--- src/main/java/com/supinfo/jee/casino/web/GreetingsController.java
+++ src/main/java/com/supinfo/jee/casino/web/GreetingsController.java
@@ -3,15 +3,31 @@ package com.supinfo.jee.casino.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingsController {
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+    @GetMapping("/dice")
+    public String greeting(@RequestParam(name = "pseudo", required = false, defaultValue = "gambler") String name, Model model) {
+        model.addAttribute("pseudo", name);
+        return "dice";
+    }
+
+    @PostMapping("/dicestartermng")
+    public String diceStartManagement(@ModelAttribute DiceStarter diceStarter, Model model) {
+        // call backend to retrieve next step to take
+        model.addAttribute("pseudo", diceStarter.getPseudo());
+        return "credits";
+    }
+
+    @PostMapping("/addcredits")
+    public String creditsManagement(@ModelAttribute Credits credits, Model model) {
+        // call backend to retrieve next step to take
+        model.addAttribute("amount", credits.getAmount());
+        return "dice";
     }
 
 }
