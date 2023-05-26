1699c4a87a11d1da47f83354298c84e8cedfb1bb
gedeffe <barbierg.gabriel@gmail.com>
2023-05-26 11:40:43 +0200

    :fire: clean temporary page

--- src/main/java/com/supinfo/jee/casino/web/CasinoController.java
+++ src/main/java/com/supinfo/jee/casino/web/CasinoController.java
@@ -10,12 +10,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class CasinoController {
 
-    @GetMapping("/dice")
-    public String greeting(@RequestParam(name = "pseudo", required = false, defaultValue = "gambler") String name, Model model) {
-        model.addAttribute("pseudo", name);
-        return "dice";
-    }
-
     @PostMapping("/dicestartermng")
     public String diceStartManagement(@ModelAttribute DiceStarter diceStarter, Model model) {
         // call backend to retrieve next step to take
@@ -27,7 +21,7 @@ public class CasinoController {
     public String creditsManagement(@ModelAttribute Credits credits, Model model) {
         // call backend to retrieve next step to take
         model.addAttribute("amount", credits.getAmount());
-        return "dice";
+        return "redirect:/";
     }
 
     @GetMapping("/connexion")
