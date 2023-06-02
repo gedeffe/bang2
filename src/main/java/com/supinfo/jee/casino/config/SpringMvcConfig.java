5a28387dafb41559af0ef81b06089305d7745790
gedeffe <barbierg.gabriel@gmail.com>
2023-06-02 10:42:15 +0200

    :art: move every static resource to a subfolder or to templates

---
+++ src/main/java/com/supinfo/jee/casino/config/SpringMvcConfig.java
@@ -0,0 +1,16 @@
+package com.supinfo.jee.casino.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index").setViewName("index");
+//        registry.addViewController("/login").setViewName("login");
+    }
+
+}
