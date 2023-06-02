46de78477c0dabce26f49cc1f254be5f87347ef4
gedeffe <barbierg.gabriel@gmail.com>
2023-06-02 10:02:57 +0200

    :bug: add missing constructor for serialization

--- src/main/java/com/supinfo/jee/casino/api/LaunchOutputDto.java
+++ src/main/java/com/supinfo/jee/casino/api/LaunchOutputDto.java
@@ -1,10 +1,12 @@
 package com.supinfo.jee.casino.api;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class LaunchOutputDto {
     private String pseudo;
     private long newBalance;
