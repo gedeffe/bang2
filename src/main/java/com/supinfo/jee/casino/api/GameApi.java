bda2e149a3746c1ce853089bf66aad971843980a
gedeffe <barbierg.gabriel@gmail.com>
2023-06-12 09:43:16 +0200

    :bug: fix post mapping

--- src/main/java/com/supinfo/jee/casino/api/GameApi.java
+++ src/main/java/com/supinfo/jee/casino/api/GameApi.java
@@ -3,7 +3,6 @@ package com.supinfo.jee.casino.api;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,6 +13,6 @@ public interface GameApi {
     @ResponseStatus(HttpStatus.CREATED)
     EntityModel<GameOutputDto> newGame(@RequestBody GameInputDto newGame);
 
-    @GetMapping("/authenticates")
+    @PostMapping("/authenticates")
     void authenticate(@RequestBody GameInputDto newGame);
 }
