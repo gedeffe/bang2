8b722d24da7242d17abbaddf0f39224605f629da
gedeffe <barbierg.gabriel@gmail.com>
2024-01-09 14:26:34 +0100

    :sparkles: add trip feature
    we've created the panel to display the add trip feature.

--- src/main/java/agency/TripAgency.java
+++ src/main/java/agency/TripAgency.java
@@ -26,6 +26,9 @@ public class TripAgency extends JFrame {
         JPanel addPlacePanel = this.displayAddPlace();
         this.add(addPlacePanel, BorderLayout.NORTH);
 
+        JPanel addTripPanel = this.displayAddTrip();
+        this.add(addTripPanel, BorderLayout.SOUTH);
+
         this.pack();
         this.setVisible(true);
     }
@@ -43,5 +46,54 @@ public class TripAgency extends JFrame {
         return panel;
     }
 
+    private JPanel displayAddTrip() {
+        JPanel panel = new JPanel(new BorderLayout());
+        Border blackline = BorderFactory.createTitledBorder("Ajouter un voyage");
+        panel.setBorder(blackline);
+
+        JComboBox comboBoxD = new JComboBox();
+        JComboBox comboBoxA = new JComboBox();
+        JPanel insidePanel = new JPanel(new GridBagLayout());
+
+        GridBagConstraints c = new GridBagConstraints();
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 1.0;
+
+        JLabel labelD = new JLabel("From: ");
+        c.gridx = 0;
+        c.gridy = 0;
+        insidePanel.add(labelD, c);
+
+        c.gridx = 1;
+        c.gridy = 0;
+        insidePanel.add(comboBoxD, c);
+
+        JLabel labelA = new JLabel("To: ");
+        c.gridx = 0;
+        c.gridy = 1;
+        insidePanel.add(labelA, c);
+
+        c.gridx = 1;
+        c.gridy = 1;
+        insidePanel.add(comboBoxA, c);
+
+        JLabel labelP = new JLabel("Price: ");
+        c.gridx = 0;
+        c.gridy = 2;
+        insidePanel.add(labelP, c);
+
+        SpinnerModel spinnerModel = new SpinnerNumberModel(0, 0, 1000, 1);
+        JSpinner sp = new JSpinner(spinnerModel);
+        c.gridx = 1;
+        c.gridy = 2;
+        insidePanel.add(sp, c);
+
+        JButton button = new JButton("Valider");
+        panel.add(button, BorderLayout.EAST);
+
+        panel.add(insidePanel, BorderLayout.CENTER);
+        return panel;
+    }
 
 }
