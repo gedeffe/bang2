d759feb70710e68b2afb84171254bedd5c34f350
33699 <thimeo.ballesta@supinfo.com>
2024-01-25 16:18:07 +0100

    create window for list of recipe

---
+++ src/main/java/com/supinfo/recipe/recipe/RecipeListController.java
@@ -0,0 +1,41 @@
+package com.supinfo.recipe.recipe;
+
+import com.supinfo.recipe.recipe.Recipe;
+import com.supinfo.recipe.recipe.RecipeDifficulty;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
+import java.util.List;
+
+public class RecipeListController {
+
+    @FXML
+    private ChoiceBox<String> choiceBox1;
+
+    @FXML
+    private ChoiceBox<String> choiceBox2;
+
+    @FXML
+    private ChoiceBox<String> choiceBox3;
+
+
+    Recipe recipe = new Recipe("tarte a Tom", "test", List.of(), List.of(), List.of(), 1, 1, RecipeDifficulty.EASY);
+    Recipe recipe2 = new Recipe("Tomme de Tom", "test2", List.of(), List.of(), List.of(), 1, 2, RecipeDifficulty.MEDIUM);
+    Recipe recipe3 = new Recipe("crumble de Tom", "test3", List.of(), List.of(), List.of(), 1, 3, RecipeDifficulty.EASY);
+
+    @FXML
+    public void initialize() {
+        choiceBox1.getItems().addAll("asc", "desc");
+        choiceBox1.setValue("asc");
+
+        choiceBox2.getItems().addAll("asc", "desc");
+        choiceBox2.setValue("asc");
+
+        // for each enum of RecipeDifficulty, add the name to the choiceBox3
+        for (RecipeDifficulty recipeDifficulty : RecipeDifficulty.values()) {
+            choiceBox3.getItems().add(recipeDifficulty.getDisplayName());
+        }
+        choiceBox3.setValue(RecipeDifficulty.values()[0].getDisplayName());
+    }
+
+}
