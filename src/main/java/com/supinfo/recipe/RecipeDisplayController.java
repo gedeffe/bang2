0bd4bce0235260134dd422aece7efce91026fa0c
Niromash <matteo.chretien@supinfo.com>
2024-01-26 16:18:12 +0100

    Remove sample datas

--- src/main/java/com/supinfo/recipe/RecipeDisplayController.java
+++ src/main/java/com/supinfo/recipe/RecipeDisplayController.java
@@ -19,16 +19,6 @@ public class RecipeDisplayController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         RecipeModel recipeModel = new RecipeModel();
-        initializeSampleData(recipeModel);
         this.recipeListViewController.setRecipeModel(recipeModel);
-    }
-
-    private void initializeSampleData(RecipeModel recipeModel) {
-        Recipe recipe = new Recipe("tarte a Tom", "test", List.of(), List.of(), List.of(), 1, 1, RecipeDifficulty.EASY);
-        Recipe recipe2 = new Recipe("Tomme de Tom", "test2", List.of(), List.of(), List.of(), 1, 2, RecipeDifficulty.MEDIUM);
-        Recipe recipe3 = new Recipe("crumble de Tom", "test3", List.of(), List.of(), List.of(), 1, 3, RecipeDifficulty.EASY);
-        recipeModel.addRecipe(recipe);
-        recipeModel.addRecipe(recipe2);
-        recipeModel.addRecipe(recipe3);
     }
 }
