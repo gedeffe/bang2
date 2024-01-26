cdb7bc4db3aec382052afb21bced26de64ba1175
33699 <thimeo.ballesta@supinfo.com>
2024-01-26 11:38:22 +0100

    create card for list recipes

--- src/main/java/com/supinfo/recipe/RecipeApplication.java
+++ src/main/java/com/supinfo/recipe/RecipeApplication.java
@@ -8,16 +8,17 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class RecipeApplication extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(RecipeApplication.class.getResource("recipe-view.fxml"));
+
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Recipe Application");
         stage.setScene(scene);
         stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch();
     }
 }
